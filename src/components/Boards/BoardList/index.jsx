b9c1325bbaa4f6cb05c2434c6d349e46785a001f
ThorriGudmundsson <71155098+ThorriGudmundsson@users.noreply.github.com>
2020-11-25 12:24:21 +0000

    Board image and text

--- src/components/Boards/BoardList/index.jsx
+++ src/components/Boards/BoardList/index.jsx
@@ -4,17 +4,20 @@ import styles from './styles';
 import BoardThumbnail from '../BoardThumbnail';
 
 const BoardList = ({ boards }) => (
-  <View style={{ flex: 1 }}>
+  <View style={styles.listContainer}>
     <FlatList
       numColumns={1}
       data={boards}
-      renderItem={({ item: { id, name, thumbnailPhoto } }) => {
-        return (
-          <BoardThumbnail thumbnailPhoto={thumbnailPhoto} />
-        )
-      }}
+      renderItem={({ item: { id, name, thumbnailPhoto } }) => (
+        <>
+          <Text>
+            <BoardThumbnail thumbnailPhoto={thumbnailPhoto} />
+            {name}
+          </Text>
+        </>
+      )}
       keyExtractor={(board) => board.name}
-      />
+    />
   </View>
 );
 

a18ffa27ba3b527335fc1fc79fb11dc480a7211c
HBreki <hbvalsson@gmail.com>
2020-11-25 20:46:30 +0000

    minor design change

--- src/components/Boards/BoardThumbnail/styles.js
+++ src/components/Boards/BoardThumbnail/styles.js
@@ -10,12 +10,13 @@ export default StyleSheet.create({
     fontSize: 14,
     fontWeight: 'bold',
     flexDirection: 'column',
+    color: 'white'
   },
   boardThumbnailContainer: {
     flexDirection: 'row',
     width: 'auto',
     height: 'auto',
-    backgroundColor: '#99ff99',
+    backgroundColor: '#615D6C',
     borderWidth: 1,
     borderColor: 'black',
     margin: 4,

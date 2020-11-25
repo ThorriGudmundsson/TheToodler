2480f8e0db16d2c1bfcc203b6bda542c0a4f9a26
ThorriGudmundsson <71155098+ThorriGudmundsson@users.noreply.github.com>
2020-11-25 16:46:40 +0000

    List of boards visualized and fixed.

--- src/components/Boards/BoardList/styles.js
+++ src/components/Boards/BoardList/styles.js
@@ -3,5 +3,8 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
   listContainer: {
     flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    backgroundColor: '#c1f0f0',
   },
 });
--- src/components/Boards/BoardThumbnail/styles.js
+++ src/components/Boards/BoardThumbnail/styles.js
@@ -2,9 +2,22 @@ import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
   image: {
-    width: 115,
-    height: 115,
+    width: 70,
+    height: 70,
     margin: 10,
-    position: 'relative',
+  },
+  thumbnailText: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    flexDirection: 'column',
+  },
+  boardThumbnailContainer: {
+    flexDirection: 'row',
+    width: 'auto',
+    height: 'auto',
+    backgroundColor: '#99ff99',
+    borderWidth: 1,
+    borderColor: 'black',
+    margin: 4,
   },
 });

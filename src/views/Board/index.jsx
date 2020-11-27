3a83df54ef4a022e4ab70ffb8d7698b89e6fcaa7
Berglindav <berglindav@gmail.com>
2020-11-27 19:22:19 +0000

    merging

--- src/views/Board/index.jsx
+++ src/views/Board/index.jsx
@@ -11,9 +11,14 @@ class Board extends React.Component {
     selectedTaskLists: [],
     boardId: 0,
   }
-  async componentDidMount() {
+ componentDidMount() {
     const { navigation } = this.props;
     const { boardId, boardName } = navigation.params;
+    this.setState({
+      taskLists: getTaskListsByBoardId(this.props.navigation.state.params.boardId)})
+    console.log(this.state.taskLists)
+
+
   }
 
   onBoardLongPress(id) {
@@ -29,12 +34,39 @@ class Board extends React.Component {
     }
   }
 
+  removeTaskList() {
+    // TODO needs to delete also tasks linked to setInterval(function () {
+    // TODO handle stuff if all are deleted
+    const { selectedTaskLists, taskLists } = this.state;
+    console.log(this.state.selectedTaskLists)
+    console.log(this.state.taskLists)
+    // Removes Tasklists from list of lists if there ares some in selectedTaskLists
+    if (selectedTaskLists.length > 0) {
+      // inspect every selctect item
+      const removeFromBoard = selectedTaskLists.filter((item) => {
+        // loop through every selected item
+        taskLists.forEach((obj, i) => {
+          // compare with every list object
+          if (obj.id === item) {
+            // delete from board
+            taskLists.splice(i, 1);
+          }
+        });
+      });
+
+      // clean up and refreach
+      selectedTaskLists.splice(0, selectedTaskLists.length);
+      this.setState({ taskLists });
+    }
+  }
+
   render() {
-    const { tasklists, selectedTaskLists, boardId } = this.state;
+    const { taskLists, selectedTaskLists, boardId } = this.state;
     return (
       <View style={{ flex: 1 }}>
         <Toolbar hasSelectedItems={selectedTaskLists.length > 0}
         onAdd={() => this.props.navigation.navigate('NewTaskList')}
+        onRemove={() => this.removeTaskList()}
         />
         <TaskList
           onLongPress={(id) => this.onBoardLongPress(id)}
@@ -42,6 +74,7 @@ class Board extends React.Component {
             getTaskListsByBoardId(this.props.navigation.state.params.boardId)
           }
           selectedTaskLists={ selectedTaskLists }
+
         />
       </View>
     );

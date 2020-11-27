974eb78f8315a080e210173e8b4aaee4317fe6fd
Berglindav <berglindav@gmail.com>
2020-11-27 09:50:58 +0000

    Views/Newboard changes

--- src/views/NewBoard/index.jsx
+++ src/views/NewBoard/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { View, Text, TouchableHighlight } from 'react-native';
+import { View, Text, TouchableHighlight, TextInput, Image } from 'react-native';
 import Toolbar from '../../components/Toolbar';
 import NewBoardCreation from '../../components/Boards/NewBoardCreation';
 import data from '../../resources/data.json';
+import ImportImages from '../Images';
 
 function onAdd(somedata) {
   somedata.push({
@@ -14,24 +15,56 @@ function onAdd(somedata) {
 
   //console.log(somedata);
 }
+class InputComponent extends React.Component {
+  state = {
+        name: '',
+        description: ''
+    }
+    genericInputHandler(name, value) {
+        this.setState({ [ name ]: value });
+    }
 
-const NewBoard = ({ navigation: { navigate } }) =>(
-  <View>
+    render() {
+        return (
+            <View>
+                <TextInput
+                    style={{
+                      height: 40,
+                      borderColor: 'gray',
+                      borderWidth: 1
+                    }}
+                    placeholder="Name the board"
+                    value={ this.state.name}
+                    onChangeText={ text => this.genericInputHandler('name', text) } />
+              <TouchableHighlight>
+                  <Text>Add image</Text>
+              </TouchableHighlight>
+              <ImportImages />
 
-    <TouchableHighlight onPress={() => onAdd(data.boards)}>
-      <Text>New dummy board</Text>
-    </TouchableHighlight>
+              <TextInput
+                style={{
+                  height: 40,
+                  borderColor: 'gray',
+                  borderWidth: 1
+                }}
+                placeholder="Add description"
+                value={ this.state.description}
+                secureTestEntry={ true }
+                onChangeText={ text => this.genericInputHandler('description', text) } />
+                <TouchableHighlight onPress={() => onAdd(data.boards)}>
+                  <Text>New dummy board</Text>
+                </TouchableHighlight>
 
-<Text> </Text>
-    <TouchableHighlight onPress={() => navigate('Boards')}>
-    <Text>back to board</Text>
-  </TouchableHighlight>
-  <Text> </Text>
-  <Text>
-    þegar farið er til baka þarf að smella á force refrech
+            <Text> </Text>
+                <TouchableHighlight onPress={() => navigate('Boards')}>
+                <Text>back to board</Text>
+              </TouchableHighlight>
+            </View>
+        );
+    }
+}
 
-</Text>
+export default InputComponent;
 
-  </View>
-);
-export default NewBoard;
+
+// export default NewBoard;
